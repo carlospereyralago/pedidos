8987a079ae2bf574428e97d2fc73e652b3b6b3af
carlospereyralago <carlospereyralago@gmai.com>
2024-06-14 14:39:43 +0200

    Despliegue de modelo y controlador

--- api/index.js
+++ api/index.js
@@ -1,11 +1,20 @@
+//estas lineas siempre tienes qe estar---------------------------
+
 const express = require('express')
-const router = require('express').Router()
 const app = express()
 const cors = require('cors')
+const fs = require('fs')
 
 app.use(cors({ origin: ['localhost:8080'], credentials: true }))
+app.use(express.json({ limit: '10mb', extended: true }))
 
+fs.readdirSync('./src/routes/').forEach(file => {
+  require(`./src/routes/${file}`)(app)
+})
 
 app.listen(8080, () => {
   console.log(`El servidor está corriendo en el puerto 8080.`)
 })
+
+//---------------------------------------------------------------
+
