477a6dfe18660a24a74a764fc16bc7fb08d278c6
carlospereyralago <carlospereyralago@gmai.com>
2024-08-01 15:10:59 +0200

    cambios 01-08-2024

--- client/customer/auth-customer/vite.config.js
+++ client/customer/auth-customer/vite.config.js
@@ -1,7 +1,16 @@
+const { defineConfig } = require('vite')
 
-export default {
+export default defineConfig({
   base: '/cliente/login',
   server: {
     port: 5176
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    minify: 'terser',
+    rollupOptions: {
+      input: '/src/index.js'
+    }
   }
-}
+})
